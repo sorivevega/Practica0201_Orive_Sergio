d5b2adf5c167281e8a96dabae08d7fb65d81d0c9
sorivevega <sorivevega@educacion.navarra.es>
2024-10-08 18:05:20 +0200

    Finalizado Ejercicio 5

---
+++ Ejercicio05.py
@@ -0,0 +1,6 @@
+#Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
+hours = input('¿Cuantas horas has trabajado?: ')
+euros = input('¿Cuanto cobras?: ')
+x = int(hours)
+y = int(euros)
+print('lo que cobra es:',(x * y)) 
