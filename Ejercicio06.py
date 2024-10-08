de75f92a382b53c83e8b0ff1189976267bb90f07
sorivevega <sorivevega@educacion.navarra.es>
2024-10-08 18:12:17 +0200

    Finalizado Ejercicio 6

---
+++ Ejercicio06.py
@@ -0,0 +1,5 @@
+#Escribir un programa que lea un entero positivo, , introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta . La suma de los  primeros enteros positivos puede ser calculada de la siguiente forma:
+x = input('¿Que valor le quieres dar?')
+n = int(x)
+Suma = (((n * (n + 1))//(2)))
+print(Suma)
