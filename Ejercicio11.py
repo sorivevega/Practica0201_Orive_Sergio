5a2e459c0df8073cd16c99949e95b9e29a25a5c0
sorivevega <sorivevega@educacion.navarra.es>
2024-10-09 17:20:47 +0200

    Finalizado Ejercicio 11

---
+++ Ejercicio11.py
@@ -0,0 +1,12 @@
+#Imagina que acabas de abrir una nueva cuenta de ahorros que te ofrece el 4% de interés al año. Estos ahorros debido a intereses, que no se cobran hasta finales de año, se te añaden al balance final de tu cuenta de ahorros. Escribir un programa que comience leyendo la cantidad de dinero depositada en la cuenta de ahorros, introducida por el usuario. Después el programa debe calcular y mostrar por pantalla la cantidad de ahorros tras el primer, segundo y tercer años. Redondear cada cantidad a dos decimales.
+dinero = input('¿Cuanto dinero tienes depositado en tu cuenta?')
+x = float(dinero)
+p = (x - ((x * 4) / 100))
+s = (p - (p * 4) / 100)
+t = (s - (s * 4) / 100)
+round(p, 2)
+round(s, 2)
+round(t, 2)
+print('Estos son tus ingresos despues del primer año:', (p), '€')
+print('Estos son tus ingresos despues del segundo año:', (s), '€')
+print('Estos son tus ingresos despues del tercer año:', (t), '€')
